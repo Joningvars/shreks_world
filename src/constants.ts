f1def0de45fda3f5a00e4e523e33d7e31f921c50
joningvars <joningvarsson@hotmail.com>
2024-11-19 18:36:35 +0000

    shreks-world

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const  CHARACTER_DATA: Record<string, Character> = {
+    shrek: {
+      name: "Shrek",
+      description: "A lovable ogre.",
+      imageUrl: "shrek.png",
+    },
+    fiona: {
+      name: "Fiona",
+      description: "A princess with an ogre form.",
+      imageUrl: "fiona.webp",
+    },
+    donkey: {
+      name: "Donkey",
+      description: "Shrek's loyal (and talkative) friend.",
+      imageUrl: "donkey.webp",
+    },
+  };
+  
